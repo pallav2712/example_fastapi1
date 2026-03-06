11bcda1d4758f99baa664b029514cd68dbdf6419
pallav2712 <pallavsharma1994@gmail.com>
2026-03-06 15:45:36 +0530

    Changed app.post using class

---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+
+
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to my api!"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your posts"}
+
+# @app.post("/createposts")
+# def create_posts(payload: dict = Body(...)):
+#     print(payload)
+#     return {"new_post": f"title: {payload['title']} content: {payload['content']}"}
+
+@app.post("/createposts")
+def create_posts(new_post: Post):
+    print(new_post.title)
+    return {"data": "new post"}

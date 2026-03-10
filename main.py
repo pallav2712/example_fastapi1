e5bcd3af8b40c5bd5f8a32163d75cd00edca7467
pallav2712 <pallavsharma1994@gmail.com>
2026-03-10 12:31:31 +0530

    using Response, status, HTTPExceptions

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI,Response,status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
@@ -48,12 +48,18 @@ def create_posts(post: Post):
 
 
 @app.get("/posts/{id}")
-def get_posts(id: int):
+def get_posts(id: int, response: Response):
+    
     post = find_post(id)
-    print(post)
-    return{"post_detail": post}
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"post with id: {id} was not found")
+        #response.status_code = status.HTTP_404_NOT_FOUND
+        #return {'message': f'post with id: {id} was not found'}
+    return {"post_detail": post}
+    
 
-@app.get("/posts/updates/latest")
-def get_latest_post():
-    post = my_posts[len(my_posts)-1]
-    return {"detail": post}
+# @app.get("/posts/updates/latest")
+# def get_latest_post():
+#     post = my_posts[len(my_posts)-1]
+#     return {"detail": post}

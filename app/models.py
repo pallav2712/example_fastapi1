8de1a37e2df96cd87d624ce14750b5f4928bc1e9
pallav2712 <pallavsharma1994@gmail.com>
2026-03-23 13:47:11 +0530

    added timestamp column to Post

--- app/models.py
+++ app/models.py
@@ -1,5 +1,8 @@
+from sqlalchemy.sql.expression import text
+
 from sqlalchemy import Column, Integer, String, Boolean
-from sqlalchemy.sql.expression import null
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+
 from .database import Base
 
 class Post(Base):
@@ -8,4 +11,5 @@ class Post(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    published = Column(Boolean, default=True)
+    published = Column(Boolean, server_default='true', nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))

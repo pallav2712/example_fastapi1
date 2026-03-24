347eeb2e994b9919a4fdf7f200ce8404584bf65e
pallav2712 <pallavsharma1994@gmail.com>
2026-03-24 17:27:21 +0530

    made changes to put now added orm inplace of cursor also made little changes to delete

--- app/main.py
+++ app/main.py
@@ -121,27 +121,37 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     # deleted_post = cursor.fetchone()
     # conn.commit()
 
-    post = db.query(models.Post).filter(models.Post.id == id)
+    post_query = db.query(models.Post).filter(models.Post.id == id)
 
-    if post.first() == None:
+    if post_query.first() == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} does not exist")
     
-    post.delete(synchronize_session=False)
+    post_query.delete(synchronize_session=False)
+    
     db.commit()
 
     return Response(status_code=status.HTTP_204_NO_CONTENT)
  
 
 @app.put("/posts/{id}")
-def update_post(id: int, post: Post):
+def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
     
-    cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",
-                   (post.title, post.content, post.published, str(id)))
-    updated_post = cursor.fetchone()
-    conn.commit()
+    # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",
+    #                (post.title, post.content, post.published, str(id)))
+    # updated_post = cursor.fetchone()
+    # conn.commit()
+
+    post_query = db.query(models.Post).filter(models.Post.id == id)
     
-    if updated_post == None:
+    post = post_query.first()
+
+    if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} does not exist")
-    return{"data": updated_post}
+    
+    post_query.update(updated_post.model_dump(), synchronize_session=False)
+
+    db.commit()
+
+    return{"data": post_query.first()}

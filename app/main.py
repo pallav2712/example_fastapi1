46af35e28c9689fe3eb606e682cf29c9eb9cc0ca
pallav2712 <pallavsharma1994@gmail.com>
2026-03-27 02:31:51 +0530

    cleared the clutter mostly the initial list code And also moved code to utils.py(hash)

--- app/main.py
+++ app/main.py
@@ -3,19 +3,18 @@ from typing import Optional, List
 from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
 from pydantic import BaseModel
-from passlib.context import CryptContext
 from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 from sqlalchemy.orm import Session
-from . import models, schemas
+from . import models, schemas, utils
 from .database import engine, get_db
 
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 models.Base.metadata.create_all(bind=engine)    # type: ignore
+
 
 app = FastAPI()
 
@@ -33,33 +32,14 @@ while True:
         print("Error: ", error)   
         time.sleep(2)
         
-
-my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1},
-{"title": "favourite foods", "content": "I like pizza", "id": 2}]   
-
-def find_post(id):
-    for p in my_posts:
-        if p["id"] == id:
-            return p
-        
-def find_index_post(id):
-    for i,p in enumerate(my_posts):
-        if p["id"] == id:
-            return i
         
 
 @app.get("/")
 def root():
     return {"message": "Welcome to my api!"}
 
-#sqlalchemy
-@app.get("/sqlalchemy")
-def test_post(db: Session = Depends(get_db)):
 
-    posts = db.query(models.Post).all()
-
-    return{"status": posts}
-
+#get
 @app.get("/posts", response_model=List[schemas.Post])
 def get_posts_o(db: Session = Depends(get_db)):
     # posts = cursor.execute("""SELECT * FROM posts""")
@@ -68,11 +48,8 @@ def get_posts_o(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
     return posts
 
-# @app.post("/createposts")
-# def create_posts(payload: dict = Body(...)):
-#     print(payload)
-#     return {"new_post": f"title: {payload['title']} content: {payload['content']}"}
 
+#post
 @app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
 def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     # cursor.execute("""INSERT INTO posts (title, content, published)
@@ -81,17 +58,14 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     # new_post = cursor.fetchone()
     # conn.commit()
     
-    #print(**post.model_dump())
-    #new_post = models.Post(title=post.title, content=post.content, published=post.published) #or
-    
     new_post = models.Post(**post.model_dump())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
-
     return new_post
 
 
+#getone
 @app.get("/posts/{id}")
 def get_posts(id: int, db: Session = Depends(get_db)):
     # cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(id)))
@@ -102,16 +76,10 @@ def get_posts(id: int, db: Session = Depends(get_db)):
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f"post with id: {id} was not found")    
-    
     return post
     
 
-@app.get("/posts/updates/latest")
-def get_latest_post():
-    post = my_posts[len(my_posts)-1]
-    return {"detail": post}
-
-
+#delete
 @app.delete("/posts/{id}", status_code= status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db)):
     # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (str(id)))
@@ -127,10 +95,10 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     post_query.delete(synchronize_session=False)
     
     db.commit()
-
     return Response(status_code=status.HTTP_204_NO_CONTENT)
  
 
+#updateall
 @app.put("/posts/{id}")
 def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
     
@@ -150,20 +118,20 @@ def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends
     post_query.update(updated_post.model_dump(), synchronize_session=False)
 
     db.commit()
-
     return post_query.first()
+
+
 
 #users
 @app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
     #hash the password which can be retrived from the user.password
-    hashed_password = pwd_context.hash(user.password)
+    hashed_password = utils.hash(user.password)
     user.password = hashed_password
 
     new_user = models.User(**user.model_dump())
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
-
     return new_user

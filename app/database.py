56794bd90e4ba4b117a0250bdb0937b99abaed1e
pallav2712 <pallavsharma1994@gmail.com>
2026-03-27 14:22:54 +0530

    made post and user file in routers and moved respective code there from the main file and also made router route instead of @app

--- app/database.py
+++ app/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+
 SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:051727@localhost/fastapi'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
@@ -11,6 +12,8 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
+
+#def get_db()-> Generator[Session, None, None]: #added by chatgpt
 
 def get_db():
     db = SessionLocal()
--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ import time
 from sqlalchemy.orm import Session
 from . import models, schemas, utils
 from .database import engine, get_db
-
+from .routers import post, user
 
 
 models.Base.metadata.create_all(bind=engine)    # type: ignore
@@ -39,99 +39,7 @@ def root():
     return {"message": "Welcome to my api!"}
 
 
-#get
-@app.get("/posts", response_model=List[schemas.Post])
-def get_posts_o(db: Session = Depends(get_db)):
-    # posts = cursor.execute("""SELECT * FROM posts""")
-    # posts = cursor.fetchall()
-
-    posts = db.query(models.Post).all()
-    return posts
+app.include_router(post.router)
+app.include_router(user.router)
 
 
-#post
-@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
-    # cursor.execute("""INSERT INTO posts (title, content, published)
-    #                 VALUES (%s, %s, %s) RETURNING *""",
-    #                 (post.title, post.content, post.published))
-    # new_post = cursor.fetchone()
-    # conn.commit()
-    
-    new_post = models.Post(**post.model_dump())
-    db.add(new_post)
-    db.commit()
-    db.refresh(new_post)
-    return new_post
-
-
-#getone
-@app.get("/posts/{id}")
-def get_posts(id: int, db: Session = Depends(get_db)):
-    # cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(id)))
-    # post = cursor.fetchone()
-    
-    post = db.query(models.Post).filter(models.Post.id == id).first() # type: ignore
-
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
-                            detail=f"post with id: {id} was not found")    
-    return post
-    
-
-#delete
-@app.delete("/posts/{id}", status_code= status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db)):
-    # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (str(id)))
-    # deleted_post = cursor.fetchone()
-    # conn.commit()
-
-    post_query = db.query(models.Post).filter(models.Post.id == id) # type: ignore
-
-    if post_query.first() == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"post with id: {id} does not exist")
-    
-    post_query.delete(synchronize_session=False)
-    
-    db.commit()
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
- 
-
-#updateall
-@app.put("/posts/{id}")
-def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
-    
-    # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",
-    #                (post.title, post.content, post.published, str(id)))
-    # updated_post = cursor.fetchone()
-    # conn.commit()
-
-    post_query = db.query(models.Post).filter(models.Post.id == id)  # type: ignore
-    
-    post = post_query.first()
-
-    if post == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"post with id: {id} does not exist")
-    
-    post_query.update(updated_post.model_dump(), synchronize_session=False)
-
-    db.commit()
-    return post_query.first()
-
-
-
-#users
-@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-
-    #hash the password which can be retrived from the user.password
-    hashed_password = utils.hash(user.password)
-    user.password = hashed_password
-
-    new_user = models.User(**user.model_dump())
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user

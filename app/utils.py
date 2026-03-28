7d765e26e883b0b8868a7d8e71fddaee3a2514a7
pallav2712 <pallavsharma1994@gmail.com>
2026-03-28 19:32:17 +0530

    added oauth2 and auth file and most importantaly made token

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,9 @@
 from passlib.context import CryptContext
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto") #see deprecated
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+# deprecated Matlab:
+# agar future mein algorithm change ho (bcrypt → kuch aur)
+# toh old hashes ko automatically upgrade kar sakta hai
 def hash(password: str):
     return pwd_context.hash(password)
 

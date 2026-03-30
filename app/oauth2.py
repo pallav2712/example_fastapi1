917af33055760fd0654ac6c555ed32a3e9034ca4
pallav2712 <pallavsharma1994@gmail.com>
2026-03-30 13:54:32 +0530

    added oauth2 file and made token verification function and used it on get post delete put methods

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,5 +1,12 @@
+from typing import List
+
 from jose import JWTError, jwt
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
+from fastapi import Depends, status, HTTPException
+from . import schemas
+from fastapi.security import OAuth2PasswordBearer
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
 #SECRET_KEY
 #Algorithm
@@ -7,15 +14,38 @@ from datetime import datetime, timedelta
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 def create_access_token(data: dict):
     to_encode = data.copy() 
-
-    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+                            #utcnow(deprecated)
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire}) #payload
-
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM) 
     #token(payload, seckey, header(alg, tokentype))
 
     return encoded_jwt
+
+
+def verify_access_token(token: str, credentials_exception):    
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])   #put algo in list?
+        id: int|None = payload.get("user_id")
+
+        if id is None:
+            raise credentials_exception
+        
+        token_data = schemas.TokenData(id=id)
+    
+    except JWTError:
+        raise credentials_exception
+
+    return token_data
+    
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                          detail=f"Could not validate credentials",
+                                            headers={"WWW-Authenticate": "Bearer"})
+    
+    return verify_access_token(token, credentials_exception)
--- app/routers/post.py
+++ app/routers/post.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
 from typing import List
-from .. import models, schemas
+from .. import models, schemas, oauth2
 from ..database import get_db
 
 router = APIRouter(
@@ -11,7 +11,7 @@ router = APIRouter(
 
 #get
 @router.get("/", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db)):
+def get_posts(db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
     # posts = cursor.execute("""SELECT * FROM posts""")
     # posts = cursor.fetchall()
 
@@ -19,25 +19,10 @@ def get_posts(db: Session = Depends(get_db)):
     return posts
 
 
-#post
-@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
-    # cursor.execute("""INSERT INTO posts (title, content, published)
-    #                 VALUES (%s, %s, %s) RETURNING *""",
-    #                 (post.title, post.content, post.published))
-    # new_post = cursor.fetchone()
-    # conn.commit()
-    
-    new_post = models.Post(**post.model_dump())
-    db.add(new_post)
-    db.commit()
-    db.refresh(new_post)
-    return new_post
-
-
 #getone
 @router.get("/{id}")
-def get_post(id: int, db: Session = Depends(get_db)):
+def get_post(id: int, db: Session = Depends(get_db),
+             user_id: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(id)))
     # post = cursor.fetchone()
     
@@ -47,11 +32,30 @@ def get_post(id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f"post with id: {id} was not found")    
     return post
-    
+
+
+#post
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db),
+                  user_id: int = Depends(oauth2.get_current_user)): #added user_id for token
+    # cursor.execute("""INSERT INTO posts (title, content, published)
+    #                 VALUES (%s, %s, %s) RETURNING *""",
+    #                 (post.title, post.content, post.published))
+    # new_post = cursor.fetchone()
+    # conn.commit()
+    print(user_id)
+
+    new_post = models.Post(**post.model_dump())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return new_post
+
 
 #delete
 @router.delete("/{id}", status_code= status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db)):
+def delete_post(id: int, db: Session = Depends(get_db),
+                user_id: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (str(id)))
     # deleted_post = cursor.fetchone()
     # conn.commit()
@@ -71,22 +75,23 @@ def delete_post(id: int, db: Session = Depends(get_db)):
 
 #updateall
 @router.put("/{id}")
-def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db),
+                user_id: int = Depends(oauth2.get_current_user)):
     
     # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",
     #                (post.title, post.content, post.published, str(id)))
     # updated_post = cursor.fetchone()
     # conn.commit()
 
-    post_query = db.query(models.Post).filter(models.Post.id == id)  
+    post_query = db.query(models.Post).filter(models.Post.id == id) #sql level  query object
     
-    post = post_query.first()
+    post = post_query.first() #python orm object
 
     if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} does not exist")
     
-    post.update(updated_post.model_dump(), synchronize_session=False) 
+    post_query.update(updated_post.model_dump(), synchronize_session=False)  # type: ignore
 
     db.commit()
     return post_query.first()

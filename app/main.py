e188552ecfda6c255d3e2e24e6612410acd4891b
pallav2712 <pallavsharma1994@gmail.com>
2026-03-31 15:28:36 +0530

    cleanup in main file

--- app/main.py
+++ app/main.py
@@ -1,48 +1,21 @@
-from multiprocessing import synchronize
-from typing import Optional, List
-from fastapi import FastAPI, Response, status, HTTPException, Depends
-from fastapi.params import Body
-from pydantic import BaseModel
-from random import randrange
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import time
-from sqlalchemy.orm import Session
-
+from fastapi import FastAPI
 from app.routers import auth
-from . import models, schemas, utils
-from .database import engine, get_db
+from . import models
+from .database import engine
 from .routers import post, user, auth
 
 
 models.Base.metadata.create_all(bind=engine)    
 
-
 app = FastAPI()
-
-
-# while True:
-
-#     try:
-#         conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres',
-#                                     password='051727', cursor_factory=RealDictCursor)
-#         cursor = conn.cursor()
-#         print("Database connection was sucessfull!")
-#         break
-#     except Exception as error:
-#         print("Connecting to database failed")
-#         print("Error: ", error)   
-#         time.sleep(2)
-        
-        
-
-@app.get("/")
-def root():
-    return {"message": "Welcome to my api!"}
-
 
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 
 
+
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to my api!"}

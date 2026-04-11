881414eecd56fc75c3c6468ffa05d94ab1a149b5
pallav2712 <pallavsharma1994@gmail.com>
2026-04-11 15:57:36 +0530

    added requirements.txt

--- app/models.py
+++ app/models.py
@@ -24,7 +24,7 @@ class User(Base):
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
-
+    phone_number = Column(String)
     # def __str__(self):
     #     return "apple" + str(self.id) # added zuk  # declares type
 

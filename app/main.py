113334591424bbc9ffdb25d53a8738aef98f26d9
pallav2712 <pallavsharma1994@gmail.com>
2026-04-11 17:32:20 +0530

    updated message in root

--- app/main.py
+++ app/main.py
@@ -32,4 +32,4 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "Welcome to my api!"}
+    return {"message": "I have done this bro deployed through render my app is live now!"}

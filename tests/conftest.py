9af2d13775275dba17561c16f9fb83def6f89836
pallav2712 <pallavsharma1994@gmail.com>
2026-04-30 03:33:08 +0530

    changed again acc to t

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,9 +12,7 @@ from app import models
 
 
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.
-database_password}@{settings.database_hostname}:{settings.database_port}/{settings.
-database_name}_test"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
--- tests/database.py
+++ tests/database.py
@@ -10,7 +10,9 @@ from app.database import Base
 
 
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.
+database_password}@{settings.database_hostname}:{settings.database_port}/{settings.
+database_name}_test"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
